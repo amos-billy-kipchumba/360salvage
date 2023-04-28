2a73de5497d735fd44c38df0913cdab67ca7dcc5
Amos Billy <amosbillykipchumba@gmail.com>
2023-04-28 11:07:07 +0300

    second of today

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -39,12 +39,12 @@ function About() {
 
         <div className='w-full flex flex-col md:flex-row bg-white px-[10px] md:px-[100px] py-[100px] gap-[25px]'>
             <div className='flex flex-1 flex-col'>
-                <h2 className=''>Simplified. Seamless. Sustainable</h2>
+                <h3 className=''>Simplified. Seamless. Sustainable</h3>
                 <p className=' text-gray-700 mt-[20px]'>At 360 Salvage we're more than just a salvage company. We’re a team of experts on a journey of growth and development, dedicated to helping shape the vehicle recycling industry. We understand that dealing with salvage vehicles can be a headache, which is why we've taken care of the entire process for you, providing simplified and seamless customer experience across our end-to-end vehicle remarketing services, that don’t break the bank.</p>
                 <p className='mt-[20px]'>Our services are designed for those who are looking for high-quality used car parts and those who want to maximize the financial and environmental benefits of their vehicles. Whether you're a car enthusiast looking for a specific part or a business owner seeking to manage your fleet's end-of-life vehicles, at 360 Salvage, we're committed to providing top-tier service and support to meet all of your salvage needs.</p>
-                <h2 className='mt-[20px]'>Our Mission</h2>
+                <h3 className='mt-[20px]'>Our Mission</h3>
                 <p className='mt-[20px]'>At 360 Salvage, our mission is to revolutionize the salvage industry by providing environmentally sustainable solutions that maximize value for our customers. We are committed to utilizing the latest technology and best practices to ensure that the salvage process is as efficient and eco-friendly as possible. We believe that by prioritizing sustainability and innovation, we can make a positive impact on the industry and the planet.</p>
-                <h2 className='mt-[20px]'>Our Values</h2>
+                <h3 className='mt-[20px]'>Our Values</h3>
                 <h3 className='mt-[20px]'>Driven by Values: Our Commitment to Sustainability, Innovation, Customer Satisfaction, Transparency, and Expertise</h3>
                 <p className='mt-[20px]'>At 360 Salvage, we're guided by a set of core values that define who we are and how we 
                 do business. We're committed to providing clear and concise information to our customers, using plain language and visuals to help them understand the process.
@@ -83,7 +83,7 @@ function About() {
         
 
         <div className='bg-white w-full flex flex-col py-[50px] px-[10px] lg:px-[100px]'>
-            <h2 className='mx-auto text-gray-600'>Our Team</h2>
+            <h3 className='mx-auto text-gray-600'>Our Team</h3>
             <span className='w-[100px] flex flex-col text-green-400 Rely mt-[20px] mx-auto'></span>
 
             <h3 className='mt-[20px]'>Driving Quality Service</h3>
@@ -95,14 +95,14 @@ function About() {
             <p className='mx-auto mt-[20px]'>We believe that the success of our business lies in the hands of our team. That's why we invest in ongoing training and development to ensure that our team members are up-to-date with the latest industry trends and technologies. With 360 Salvage, you can trust that you are working with a team of professionals who are passionate about what they do and are committed to delivering the best possible experience to our clients.</p>
 
 
-            <h2 className='mx-auto text-gray-600'>360 Salvage and Sustainability</h2>
+            <h3 className='mx-auto text-gray-600'>360 Salvage and Sustainability</h3>
 
             <h3 className='mt-[20px]'>Journey to the Future</h3>
 
             <p className='mx-auto mt-[20px]'>At 360 Salvage, we prioritize sustainability in all our operations. Our approach to sustainability is outlined in our Journey to the Future, which sets out our goals, commitments, and progress towards becoming a more sustainable business. Our focus is on three key areas: Our Environment, Our Team, and Our Collaborations, and our Journey to the Future guides our efforts towards achieving our sustainability goals both in the short and long term.</p>
 
 
-            <h2 className='mx-auto text-gray-600'>WHAT WE DO</h2>
+            <h3 className='mx-auto text-gray-600'>WHAT WE DO</h3>
 
             <h3 className='mt-[20px]'>Simplified and Seamless End-to-End Vehicle Remarketing Solutions</h3>
 
@@ -111,7 +111,7 @@ function About() {
         </div>
 
         <div className='bg-white w-full flex flex-col py-[10px] px-[10px] lg:px-[100px]'>
-            <h2 className='mx-auto text-gray-600'>Our Services</h2>
+            <h3 className='mx-auto text-gray-600'>Our Services</h3>
 
             <p className='mx-auto mt-[20px]'>Our business activities focus on the following capabilities.</p>
 
@@ -131,58 +131,58 @@ function About() {
         </div>
 
 
-        <div className='bg-white mx-auto px-4 xl:px-[100px] py-6 flex w-full flex-wrap gap-[5%]'>
+        <div className='bg-white mx-auto px-4 xl:px-[100px] py-6 flex w-full flex-wrap gap-[5%] gap-y-[30px]'>
             <div className='flex flex-col min-w-[90%] lg:min-w-[30%] lg:max-w-[30%] mt-[20px] xl:mt-[0px]'>
-                <h3>CAR AUDIO SERVICE</h3>
+                <h4>CAR AUDIO SERVICE</h4>
 
                 <div className='flex gap-[20px] mt-[20px]'>
                     <span className=''><Speaker /></span>
-                    <h4>Enhance your driving experience with our professional car audio service.</h4>
+                    <p>Enhance your driving experience with our professional car audio service.</p>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1 min-w-[90%] lg:min-w-[30%] lg:max-w-[30%] mt-[20px] xl:mt-[0px]'>
-                <h3>FREE OIL CHANGE</h3>
+                <h4>FREE OIL CHANGE</h4>
 
                 <div className='flex gap-[20px] mt-[20px]'>
                     <span className=''><OilBarrel /></span>
-                    <h4>Get a free oil change with any service appointment at 360 Salvage.</h4>
+                    <p>Get a free oil change with any service appointment at 360 Salvage.</p>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1 min-w-[90%] lg:min-w-[30%] lg:max-w-[30%] mt-[20px] xl:mt-[0px]'>
-                <h3>ENGINE DIAGNOSTICS</h3>
+                <h4>ENGINE DIAGNOSTICS</h4>
 
                 <div className='flex gap-[20px] mt-[20px]'>
                     <span className=''><DisplaySettings /></span>
-                    <h4>Keep your engine running smoothly with our expert engine diagnostics.</h4>
+                    <p>Keep your engine running smoothly with our expert engine diagnostics.</p>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1 min-w-[90%] lg:min-w-[30%] lg:max-w-[30%]'>
-                <h3>CAR A/C RECHARGE</h3>
+                <h4>CAR A/C RECHARGE</h4>
 
                 <div className='flex gap-[20px] mt-[20px]'>
                     <span className=''><ChargingStation /></span>
-                    <h4>Stay cool and comfortable during the summer with our car A/C recharge</h4>
+                    <p>Stay cool and comfortable during the summer with our car A/C recharge</p>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1 min-w-[90%] lg:min-w-[30%] lg:max-w-[30%]'>
-                <h3>PARKING SENSORS CALIBRATION</h3>
+                <h4>PARKING SENSORS CALIBRATION</h4>
 
                 <div className='flex gap-[20px] mt-[20px]'>
                     <span className=''><SensorOccupied /></span>
-                    <h4>Ensure your safety while parking with our parking sensors calibration service..</h4>
+                    <p>Ensure your safety while parking with our parking sensors calibration service..</p>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1 min-w-[90%] lg:min-w-[30%] lg:max-w-[30%]'>
-                <h3>CAR BATTERY REPAIRS</h3>
+                <h4>CAR BATTERY REPAIRS</h4>
 
                 <div className='flex gap-[20px] mt-[20px]'>
                     <span className=''><CarRepair /></span>
-                    <h4>Keep your car battery in top condition with our expert repair services.</h4>
+                    <p>Keep your car battery in top condition with our expert repair services.</p>
                 </div>
             </div>
         </div>
